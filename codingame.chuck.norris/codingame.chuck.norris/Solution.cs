4460a7ee5a7dfc8fdf7d46391ac73ad3396df09b
alinddicu <alinddicu@yahoo.com>
2016-03-10 16:02:11 +0100

    tu failed

---
+++ codingame.chuck.norris/codingame.chuck.norris/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace codingame.chuck.norris
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            string MESSAGE = Console.ReadLine();
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine("answer");
+        }
+    }
+
+    public class ChuckNorrisBinaryConverter
+    {
+ 
+    }
+}
